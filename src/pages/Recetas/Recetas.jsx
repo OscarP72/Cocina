e5f3e731bb8ca0aedc50c748fae59d7d5298f13a
Your Name <you@example.com>
2023-10-19 22:49:52 +0200

    a

--- src/pages/Recetas/Recetas.jsx
+++ src/pages/Recetas/Recetas.jsx
@@ -5,13 +5,17 @@ import { useState, useEffect } from "react";
 const Recetas = ()=>{
 const [items, setItems] = useState([]);
 const [loaded, setLoaded] = useState (false);
-
+const [page, setPage]= useState(0);
 
 
 const getItems =async () =>{
-    const data = await fetch("https://api.edamam.com/api/recipes/v2?type=public&app_id=9e9e1d2c&app_key=ffef92e20691c00963fe7a041e762e09&diet=balanced&imageSize =NORMALES");
+    const YOUR_APP_ID="9e9e1d2c";
+    const YOUR_APP_KEY="ffef92e20691c00963fe7a041e762e09";
+    const query= "q:";
+    
+    const data = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&page=${page}&limit=30`);
     const dataJSON = await data.json();
-    setItems(dataJSON.tags);
+    setItems(dataJSON.hits);
 };
 useEffect(() => {
     getItems();
@@ -19,6 +23,20 @@ useEffect(() => {
 
 
 
-    return <main>Recetas</main>
+    return <main className="recetario">
+        <ul className="galeria">
+            {items.map((item)=> 
+            <li key={item.source}>
+                <img src={item.recipe.image} alt={item.recipe.label}/>
+                <h4>{item.recipe.title}</h4>
+                <h2>{item.recipe.label}</h2>
+                <h4>{item.recipe.ingredientLines}</h4>
+               
+                
+                
+               
+            </li>)}
+        </ul>
+    </main>
 }
 export default Recetas;
